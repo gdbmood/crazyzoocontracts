caf88daf422fd85d583c057b2b6e175c1e368e43
ahmedraza90 <34ahmed.raza@gmail.com>
2023-05-16 01:15:54 +0500

    completed

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,11 +7,11 @@ module.exports = {
   networks: {
     mumbai: {
       url: "https://polygon-mumbai.g.alchemy.com/v2/Qg-FtBXZr0sZeieqGOg1HoR1ma6MscWF",
-      accounts: [`0x73d96a1f681f84fa872fe740e727a8aec8f54c341b213eb7323af6331344aaa8`],
+      accounts: [`place your private key here`],
     },
     arbitrum: {
       url: "https://arb-goerli.g.alchemy.com/v2/pCXGTOX5cerPI87r5_a_IGJsclHxk6kj",
-      accounts: [`0x73d96a1f681f84fa872fe740e727a8aec8f54c341b213eb7323af6331344aaa8`],
+      accounts: [`place your private key here`],
     },
   },
 };
